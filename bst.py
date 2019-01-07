2609999701c83d6b91bff46263c72fc460702408
aditi148 <adidevi.pawar2@gmail.com>
2019-01-08 00:00:10 +0530

    Update bst.py

--- bst.py
+++ bst.py
@@ -17,5 +17,21 @@ class node:
 				self.right.insert(data)
 		else:
 			self.data = data
-			
+	
+	def lookup(self, data, parent= None):
+		if data < self.data:
+			if self.left is None:
+				return None, None
+			return self.left.lookup(data, self)
+		elif data > self.data:
+			if self.rightt is None:
+				return None, None
+			return self.right.lookup(data, self)
+		else:
+			return self, parent
+		
+	def delete(self, data):
+		
+		
+		
 			
